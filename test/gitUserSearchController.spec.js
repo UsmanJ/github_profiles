a79705d16053995d8fa285acf22c7592028fd2c6
Usman Jamil <usman.jamil@live.co.uk>
2015-10-21 10:56:04 +0100

    added GitHub API

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -1,11 +1,12 @@
+beforeEach(module('GitUserSearch'));
+
+var ctrl;
+
+beforeEach(inject(function($controller) {
+  ctrl = $controller('GitUserSearchController');
+}));
+
 describe('GitUserSearchController', function() {
-  beforeEach(module('GitUserSearch'));
-
-  var ctrl;
-
-  beforeEach(inject(function($controller) {
-    ctrl = $controller('GitUserSearchController');
-  }));
 
   it('initialises with an empty search result and term', function() {
     expect(ctrl.searchResult).toBeUndefined();
@@ -14,6 +15,16 @@ describe('GitUserSearchController', function() {
 });
 
 describe('when searching for a user', function() {
+
+  var httpBackend;
+  beforeEach(inject(function($httpBackend) {
+    httpBackend = $httpBackend
+    httpBackend
+      .when("GET", "https://api.github.com/search/users?q=hello")
+      .respond(
+        { items: items }
+      );
+  }));
 
   var items = [
     {
@@ -31,6 +42,7 @@ describe('when searching for a user', function() {
   it('displays search results', function() {
     ctrl.searchTerm = 'hello';
     ctrl.doSearch();
+    httpBackend.flush();
     expect(ctrl.searchResult.items).toEqual(items);
   });
 });
