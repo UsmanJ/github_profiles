2de69abcf87cf0b1a571cbed202a20aa9c8189a4
Usman Jamil <usman.jamil@live.co.uk>
2015-10-22 10:03:46 +0100

    modified server.js

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
-
-var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req, res) {
   res.sendfile('./index.html');
-});
-
-app.listen(port, function() {
-  console.log('Our app is running on http://localhost:' + port);
 });
 
 module.exports = app;
