9f62d6ae8704e662a992db9ed7f1a4a4337c0131
Usman Jamil <usman.jamil@live.co.uk>
2015-10-22 11:59:37 +0100

    changed .json file

--- server.js
+++ server.js
@@ -8,4 +8,10 @@ app.get('/', function(req, res) {
   res.sendfile('./index.html');
 });
 
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+  console.log('Our app is running on http://localhost:' + port);
+});
+
 module.exports = app;
