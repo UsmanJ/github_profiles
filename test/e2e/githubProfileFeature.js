b69c4e624174cb85f494c3521061c372a3668d64
Usman Jamil <usman.jamil@live.co.uk>
2015-10-22 16:37:14 +0100

    protractor pill completed and all tests passing

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,7 +1,37 @@
-describe('Github Profile finder', function() {
-  it('has a title', function() {
-    browser.get('http://localhost:8080');
+describe('GitHub profile finder', function() {
 
+  var searchBox = element(by.model('searchCtrl.searchTerm'))
+  var searchButton = element(by.className('btn'))
+
+  beforeEach(function() {
+    browser.get('http://localhost:8080');
+  })
+
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual('Github user search');
   });
+
+  it('finds profiles', function() {
+    searchBox.sendKeys('spike01');
+    searchButton.click();
+    expect(element(by.binding('user.login')).getText()).
+      toEqual('spike01');
+  });
+
+  it('finds profiles', function() {
+
+    searchBox.sendKeys('spike');
+    searchButton.click();
+
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.get(1).getText()).toEqual('spike01');
+  });
+
+  it('finds the last Spike', function() {
+   searchBox.sendKeys('spike');
+   searchButton.click();
+
+   var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+   expect(profiles.last().getText()).toEqual('spikelynch');
+ });
 });
